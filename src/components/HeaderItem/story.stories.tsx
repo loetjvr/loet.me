933fb46967e0e9d8bb63d9f0bf83d0d8d4c98fe7
Loet Jansen van Rensburg <loet@rawrcreations.com>
2020-12-13 02:16:07 +0900

    refeactor: move stories in components

--- src/components/HeaderItem/story.stories.tsx
+++ src/components/HeaderItem/story.stories.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
-import HeaderItem from '../components/HeaderItem';
-import '../styles/reset.css';
+import HeaderItem from '.';
+import '../../styles/reset.css';
 
 export default {
   title: 'loet.me/HeaderItem',
