ecfcdb28d64d1c2a5277142598edc9c620b194da
Loet Jansen van Rensburg <loet@rawrcreations.com>
2023-06-22 22:04:09 +0900

    Revert "feat: add vercel"
    This reverts commit 0d23f307d13108e75fae7afe9c4ed8e30be4b2b0.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 
-import vercel from "@astrojs/vercel/serverless";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), image()],
-  output: "server",
-  adapter: vercel()
+  integrations: [mdx(), sitemap(), tailwind(), image()]
 });
